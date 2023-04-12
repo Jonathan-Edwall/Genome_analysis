0501a2e8c006fd5a0344f27290a59fde52ec2ca1
Jonathan-Edwall <92929542+Jonathan-Edwall@users.noreply.github.com>
2023-04-12 20:41:52 +0200

    Added bash scripts generated from the python script

--- code/Symbolic link generator/Symbolic_link_generator_script.py
+++ code/Symbolic link generator/Symbolic_link_generator_script.py
@@ -3,19 +3,36 @@
 
 def symbolic_link_generator_fun(txt_file,path_original_file,path_symbolic_link):
     with open(txt_file, 'r') as inputfile:
-        output_file = "symbolic_links_for_" + txt_file.split(".")[0] 
+        output_file = "symbolic_links_for_" + txt_file.split(".")[0]+"script.sh" 
         with open(output_file,"w") as outfile:
                 outfile.truncate()
+                outfile.write("#!/bin/bash")
+                outfile.write("\n")
                 # Loop through each line in the file
                 for line in inputfile:
                     outfile.write("ln -s "+path_original_file+line.strip()+" "+path_symbolic_link+line.strip())
                     outfile.write("\n")
                     # Printing the command to create the symbolic link
                     # print("ln -s"+path_original_file+line.strip()+" "+path_symbolic_link+line.strip())
-    
+    print(f"*** Done! *** ")
+    print(f"*To execute the generated script in the command line, type:\n$ source {output_file}")
+
+# original_file_path = "/proj/genomeanalysis2023/Genome_Analysis/4_Tean_Teh_2017/pacbio_data/"
+# symbolic_link_path = "/home/joed4996/Genome_analysis_local_rep/data/raw_external_data/genomic_data/pacbio_data/"
+# input_file = "Pacbio_genomic_data.txt"
+# symbolic_link_generator_fun(input_file,original_file_path,symbolic_link_path)
+
+
+# original_file_path = "/proj/genomeanalysis2023/Genome_Analysis/4_Tean_Teh_2017/illumina_data/"
+# symbolic_link_path = "/home/joed4996/Genome_analysis_local_rep/data/raw_external_data/genomic_data/illumina_data/"
+# input_file = "Illumina_genomic_data.txt"
+# symbolic_link_generator_fun(input_file,original_file_path,symbolic_link_path)
+
+
+
 
 # original_file_path = "/proj/genomeanalysis2023/Genome_Analysis/4_Tean_Teh_2017/transcriptome/trimmed/"
-# symbolic_link_path = "/home/joed4996/Genome_analysis_local_rep/data/transcriptomic_data/trimmed/"
+# symbolic_link_path = "/home/joed4996/Genome_analysis_local_rep/data/raw_external_data/transcriptomic_data/trimmed/"
 # input_file = "trimmed_transcriptomic_data.txt"
 # symbolic_link_generator_fun(input_file,original_file_path,symbolic_link_path)
 
--- code/Symbolic link generator/symbolic_links_for_untrimmed_transcriptomic_datascript.sh
+++ code/Symbolic link generator/symbolic_links_for_untrimmed_transcriptomic_datascript.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ln -s /proj/genomeanalysis2023/Genome_Analysis/4_Tean_Teh_2017/transcriptome/untrimmed/SRR6040095_scaffold_06.1.fastq.gz /home/joed4996/Genome_analysis_local_rep/data/transcriptomic_data/untrimmed/SRR6040095_scaffold_06.1.fastq.gz
 ln -s /proj/genomeanalysis2023/Genome_Analysis/4_Tean_Teh_2017/transcriptome/untrimmed/SRR6040095_scaffold_06.2.fastq.gz /home/joed4996/Genome_analysis_local_rep/data/transcriptomic_data/untrimmed/SRR6040095_scaffold_06.2.fastq.gz
 ln -s /proj/genomeanalysis2023/Genome_Analysis/4_Tean_Teh_2017/transcriptome/untrimmed/SRR6040095_scaffold_10.1.fastq.gz /home/joed4996/Genome_analysis_local_rep/data/transcriptomic_data/untrimmed/SRR6040095_scaffold_10.1.fastq.gz
