4777b80d5b7d930c45d21851ec1f84bed3b5b1c3
Jonathan-Edwall <edwalljonathan@gmail.com>
2023-05-17 16:35:07 +0200

    Removed redundant r-scripts for deseq2

--- code/deseq2/deseq2_new.R
+++
@@ -1,8 +0,0 @@
-#if (!requireNamespace('BiocManager',quietly = TRUE)) 
-install.packages('BiocManager',force=TRUE)
-library('BiocManager')
-BiocManager::install('DESeq2',force=TRUE) 
-library('DESeq2')
-
-
-
